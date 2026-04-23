cf042104d14015f8088072566e3c7e958660cace
unknown <pankajbidgar07@gmail.com>
2026-04-23 16:06:09 +0530

    call update

--- src/components/VideoCall.js
+++ src/components/VideoCall.js
@@ -192,7 +192,7 @@ function VideoCall() {
                   <div className="default-avatar">👤</div>
                 )}
                 <span>{exp.full_name}</span>
-                <small>({exp.role === 'buyer' ? 'प्रशासक' : 'तज्ञ'})</small>
+                <small>({exp.role === 'buyer' ? 'खरेदीदार' : 'तज्ञ'})</small>
               </div>
             ))
           )}
