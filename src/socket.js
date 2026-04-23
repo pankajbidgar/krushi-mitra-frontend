1f1bd2abe6af0dacdf57d8ce3f7fa044bcab7a2b
unknown <pankajbidgar07@gmail.com>
2026-04-23 12:06:01 +0530

    update socket.io

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,17 @@
 
 
+// import { io } from 'socket.io-client';
+// const socket = io(process.env.REACT_APP_API_URL);
+// export default socket;
+
+
+
+
 import { io } from 'socket.io-client';
-const socket = io(process.env.REACT_APP_API_URL);
+
+const socket = io(process.env.REACT_APP_API_URL, {
+  transports: ['websocket'],   // 🔥 important fix
+  withCredentials: true,
+});
+
 export default socket;
